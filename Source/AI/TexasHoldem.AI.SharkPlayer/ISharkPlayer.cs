b3fab8bc0a3427177135d0989aa2cf83a1fdbf39
beard84 <kalin.dimchev@mail.bg>
2015-11-11 16:31:02 -0800

    Add our SharkPlayer.cs Add a bunch of TODO's

---
+++ Source/AI/TexasHoldem.AI.SharkPlayer/ISharkPlayer.cs
@@ -0,0 +1,13 @@
+﻿namespace TexasHoldem.AI.SharkPlayer
+{
+    using System;
+    using TexasHoldem.AI.SmartPlayer.Helpers;
+    using TexasHoldem.Logic;
+    using TexasHoldem.Logic.Extensions;
+    using TexasHoldem.Logic.Players;
+
+    public interface ISharkPlayer
+    {
+        PlayerAction GetTurn(GetTurnContext context);
+    }
+}
---
+++ Source/AI/TexasHoldem.AI.SharkPlayer/TightSharkPlayer.cs
@@ -0,0 +1,16 @@
+﻿namespace TexasHoldem.AI.SharkPlayer
+{
+    using System;
+    using TexasHoldem.AI.SmartPlayer.Helpers;
+    using TexasHoldem.Logic;
+    using TexasHoldem.Logic.Extensions;
+    using TexasHoldem.Logic.Players
+
+    public class TightSharkPlayer : ISharkPlayer
+    {
+        public PlayerAction GetTurn(GetTurnContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
